478e379d836118a00ed400abcea90a2b086d3618
liyinxia <liyinxia@weimiao.cn>
2024-10-30 16:54:55 +0800

    feat: 主题样式修改

--- packages/components/base/button/index.ts
+++ packages/components/base/button/index.ts
@@ -2,5 +2,6 @@
 import button from "./src/index.vue";
 import { withInstall,SFCWithInstall } from '../../../utils/with-install'
 export const CqButton:SFCWithInstall<typeof button> = withInstall(button);
+export default CqButton
 // export * from './src/index.ts'
 
