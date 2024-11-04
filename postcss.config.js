a8537244729d73a019d27acfd8bad8690121657e
liyinxia <liyinxia@weimiao.cn>
2024-11-04 15:46:21 +0800

    feat: 增加配置文件

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,13 @@
+const postImport = require('postcss-import')
+const postUrl = require('postcss-url')
+const autoprefixer = require('autoprefixer')
 
 const PostcssDiscardDuplicates = require('postcss-discard-duplicates') //去重
 module.exports = {
   plugins: [
+    autoprefixer(),
+    postImport(),
+    postUrl(),
     PostcssDiscardDuplicates(),
   ]
 };
